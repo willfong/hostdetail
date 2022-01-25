6998c6503897f68605e7123925f0e2749b5f9682
Will Fong <willfong@hey.com>
2022-01-25 15:16:01 +0800

    linter

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
 app.get("/", (req, res) => {
-    res.json({...req.headers, currentTs: new Date()});
+	res.json({ ...req.headers, currentTs: new Date() });
 });
 
-app.use(function(req, res) {
-    res.status(404).send("404: Page not Found");
+app.use(function (req, res) {
+	res.status(404).send("404: Page not Found");
 });
-  
-app.use(function(error, req, res, next) {
-    res.status(500).send("500: Internal Server Error");
+
+app.use(function (error, req, res, next) {
+	res.status(500).send("500: Internal Server Error");
 });
 
 app.listen(port, () => {
-    console.log(`Service started on port: ${port}`);
-})
+	console.log(`Service started on port: ${port}`);
+});
