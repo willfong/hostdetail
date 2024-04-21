85e1ad3d2b610dae4956b3e40f0cc91f6d2f78f3
Will Fong <willfong@hey.com>
2024-04-21 23:09:12 +0700

    remove hardcode profile

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,5 +1,4 @@
-# This isn't used yet
-AWS_PROFILE=davao aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 742334901973.dkr.ecr.ap-southeast-1.amazonaws.com
+aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 742334901973.dkr.ecr.ap-southeast-1.amazonaws.com
 docker build -t hostdetail .
 echo "Commit Hash: $(git rev-parse --short HEAD)" && docker tag hostdetail:latest 742334901973.dkr.ecr.ap-southeast-1.amazonaws.com/hostdetail:$(git rev-parse --short HEAD) && docker push 742334901973.dkr.ecr.ap-southeast-1.amazonaws.com/hostdetail:$(git rev-parse --short HEAD)
 docker tag 742334901973.dkr.ecr.ap-southeast-1.amazonaws.com/hostdetail:$(git rev-parse --short HEAD) 742334901973.dkr.ecr.ap-southeast-1.amazonaws.com/hostdetail:production
