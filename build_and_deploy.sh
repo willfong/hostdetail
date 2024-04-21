5d42b460a050c6bbd4e0e38459658c8a4a7907ef
Will Fong <willfong@hey.com>
2024-04-22 00:01:40 +0700

    Improve tooling points

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,5 +1,5 @@
 aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 742334901973.dkr.ecr.ap-southeast-1.amazonaws.com
-docker build -t hostdetail .
+docker build --platform=linux/amd64 -t hostdetail .
 echo "Commit Hash: $(git rev-parse --short HEAD)" && docker tag hostdetail:latest 742334901973.dkr.ecr.ap-southeast-1.amazonaws.com/hostdetail:$(git rev-parse --short HEAD) && docker push 742334901973.dkr.ecr.ap-southeast-1.amazonaws.com/hostdetail:$(git rev-parse --short HEAD)
 docker tag 742334901973.dkr.ecr.ap-southeast-1.amazonaws.com/hostdetail:$(git rev-parse --short HEAD) 742334901973.dkr.ecr.ap-southeast-1.amazonaws.com/hostdetail:production
 docker push 742334901973.dkr.ecr.ap-southeast-1.amazonaws.com/hostdetail:production
